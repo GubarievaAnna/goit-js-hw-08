9e21cc1d9a3c92ab33772c537bb2b7053d87f6d7
GubarievaAnna <annagubarieva@gmail.com>
2022-07-01 16:13:53 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+
+continueToWatch();
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function onPlayerTimeUpdate() {
+  player.getCurrentTime().then(seconds => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+function continueToWatch() {
+  const timeBeforeUpdate = localStorage.getItem('videoplayer-current-time');
+  if (timeBeforeUpdate) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+}
