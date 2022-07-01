4aabb3d27d7dd8ce136d72d1b220adf85108b5d0
GubarievaAnna <annagubarieva@gmail.com>
2022-07-01 07:22:08 +0300

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+function createBlockWithImage({ preview, original, description }) {
+  return `<li><a class="gallery__item" href = "${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a></li>`;
+}
+
+galleryListEl.insertAdjacentHTML(
+  'beforeend',
+  galleryItems.map(createBlockWithImage).join('')
+);
+
+const galleryLightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+  showCounter: false,
+  widthRatio: 0.6,
+});
